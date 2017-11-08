189b2e3ef1c3bec0fda872063afe0f722925143f
Syu125 <yusophia2011@gmail.com>
2017-11-07 19:25:01 -0800

    1.GreeterRecipe 2.EverythingIsAwesome 3.KnockKnockRecipe

---
+++ src/EverythingIsAwesomeRecipe.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesomeRecipe {
+
+	
+	
+	public static void main(String[] args) {
+		String name=JOptionPane.showInputDialog("What do you like");
+		JOptionPane.showMessageDialog(null,name +" are awesome");
+	
+		
+	}
+}
---
+++ src/GreeterRecipe.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class GreeterRecipe {
+
+
+
+	public static void main(String[] args) {
+		
+		String name=JOptionPane.showInputDialog("What is your name");
+		JOptionPane.showMessageDialog(null,"Hi " +name);
+		
+		
+		
+		
+		
+	}
+}
